4419d69f99508437e9cef55beecce84651fc6d06
Syeda Juveria Afreen <sja.affu765@gmail.com>
2023-01-17 22:46:26 +0300

    Update line spacing

--- 3-Rock-Paper-Scissor/rock_paper_scissor.py
+++ 3-Rock-Paper-Scissor/rock_paper_scissor.py
@@ -17,12 +17,13 @@ def play():
 
     return 'You lost!'
 
-    def is_win(player, opponent):
-        # return true if player wins
-        # r > s, s > p, p > r
-        if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-                or (player == 'p' and opponent == 'r'):
-            return True
+
+def is_win(player, opponent):
+    # return true if player wins
+    # # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+            or (player == 'p' and opponent == 'r'):
+        return True
 
 
 print(play())
